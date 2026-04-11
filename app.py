331a237e0e8c9a565dce62a7c3106e0243ba3787
mbankoti64-hue <mbankoti64@gmail.com>
2026-04-11 09:37:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,12 +61,18 @@ college_data = {
      Approved by: UGC, AICTE""",
     "courses": "B.Tech, BCA, MBA, BBA, B.Com, MCA TOTAL 100+ course (UG + PG + PHD + Diploma)" ,
     "fees": "B.Tech: ₹2.5–3.5 LPA \n BCA: ₹1.2–1.5 LPA PER YEAR",
-    "placement": "Highest: ₹65+ LPA \n Avg: ₹5–8 LPA",
+    "placement": "Highest: ₹65+ LPA  Avg: ₹5–8 LPA",
     "hostel": "WiFi, Mess, Security 24/7,ALL best  are their",
-    "hostel name": "CHANDRA SHEKHER AZAD \n  SARDAR PATEL HOSTEL \n  NETAJI SUBHASH CHANDRA HOSTEL  \n SAI HOSTEL", 
+    "hostel name": "CHANDRA SHEKHER AZAD\n  
+    SARDAR PATEL HOSTEL\n  
+    NETAJI SUBHASH CHANDRA HOSTEL\n
+    SAI HOSTEL", 
     "hostel fees ":" 160000 These fees may vary for different hostels ",
     "faculty": "600+ Faculty | PhD Holders",
-    " faculty names": "Mr. Priyansh Kumar (Introduction to Data Science)\n Mr. Mohit Amoli (Foundations of Artificial Intelligence) \n Dr.",
+    
+    " faculty names": "Mr. Priyansh Kumar (Introduction to Data Science)\n
+    Mr. Mohit Amoli (Foundations of Artificial Intelligence) \n
+    Dr.",
     
 }
 
@@ -117,13 +123,13 @@ def get_response(user_input):
     elif "placement" in user_input:
         return college_data["placement"]
     elif "hostel" in user_input:
+    elif "faculty name" in user_input:
+        return college_data["faculty name"]
         return college_data["hostel"]
     elif "faculty" in user_input:
         return college_data["faculty"]
     elif "about" in user_input or "college" in user_input:
         return college_data["about"]
-    elif "faculty name" in user_input:
-        return college_data["faculty name"]
     elif "hi" in user_input or "hello" in user_input or "namste bhai jii" in user_input:
         return "AA GYA GANDU HIIIIIIIIII KESA LGA MERA MAJAK "
     else:
